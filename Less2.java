9fc8fb0f096b84d57133438979b76c26cc0f106f
zajcev <41670509+zajcev@users.noreply.github.com>
2018-07-25 11:13:13 +0300

    Lesson #2
    Mini-Game

---
+++ Less2.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package less2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Sys_Adm
+ */
+public class Less2 {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        for (int i = 10; i <= 30; i += 10) 
+            PlayLevel(i);
+        System.out.println("WIN");
+
+        scanner.close();
+    }
+
+    private static void PlayLevel(int i) {
+        int number = (int) (Math.random() * i);
+        while (true) {
+            System.out.println("Угадай число от 0 до " + i);
+            int input_number = scanner.nextInt();
+            if (input_number == number) {
+                System.out.println("УгадаL");
+                break;
+
+            } else if (input_number > number) {
+                System.out.println("More");
+            } else if (input_number < number) {
+                System.out.println("Less");
+
+            } else {
+                System.out.println("Это не число");
+
+            }
+        }
+    }
+}
