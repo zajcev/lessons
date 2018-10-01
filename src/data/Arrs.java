1fd9460cdf4ea83aacbb07bbbd835e7670769229
zajcev <winer4ever@yandex.ru>
2018-10-01 15:10:20 +0300

    Пример обьединения двух массивов через ArrayList

---
+++ src/data/Arrs.java
@@ -0,0 +1,18 @@
+package data;
+
+import java.util.ArrayList;
+
+public class Arrs{
+public static void main(String[] args){
+    int[] i = {1,2,3};
+    int[] j = {4,5,6};
+    Integer[] display;
+
+    ArrayList<int[]> arr = new ArrayList<>();
+    arr.add(i);
+    arr.add(j);
+
+}
+}
+
+
