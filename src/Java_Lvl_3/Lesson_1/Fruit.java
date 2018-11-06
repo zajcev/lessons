97c329401a0e861da40766df57265f4806474110
zajcev <winer4ever@yandex.ru>
2018-11-06 22:04:11 +0300

    Решение дз 1-ого урока Java_Lvl_3

---
+++ src/Java_Lvl_3/Lesson_1/Fruit.java
@@ -0,0 +1,23 @@
+package Java_Lvl_3.Lesson_1;
+
+public class Fruit {
+    int weight;
+
+    public Fruit(){
+    this.weight = 0;
+    }
+}
+class Apple extends Fruit{
+
+    public Apple() {
+        super.weight = 2;
+    }
+
+}
+    class Orange extends Fruit{
+
+        public Orange() {
+            super.weight = 3;
+        }
+
+    }
